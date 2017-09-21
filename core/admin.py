4a0aac2f7856a4300e00da9edf44495f71356eec
olivx <oliveiravicente.net@gmail.com>
2017-09-21 11:06:33 -0300

    company admin and form

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from core.models import Company
+from core.forms import CompanyForm
+
 
 # Register your models here.
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    form = CompanyForm
+
+    search_fields = ('vaga',)
+    list_filter = ('distancia_max','experiencia', 'escolaridade')
+    list_display = ('vaga', 'experiencia', 'escolaridade', 'distancia_max',
+                    'salario_min', 'salario_max')
--- core/forms.py
+++ core/forms.py
@@ -6,18 +6,18 @@ from core.models import Address, Company, Candidate
 class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
-        fields = ('cliente', 'cep', 'endereco', 'numero', 'complemento',
+        fields = ('cep', 'endereco', 'numero', 'complemento',
                   'bairro', 'cidade', 'uf', 'observacao')
 
 
 class CompanyForm(forms.ModelForm):
     class Meta:
         model = Company
-        fields = ('end', 'vaga', 'salario_min', 'salario_max',
-                  'experiencia', 'escolariodade', 'distancia_max')
+        fields = ('vaga', 'salario_min', 'salario_max',
+                  'experiencia', 'escolaridade', 'distancia_max')
 
 
 class CandidateFrom(forms.ModelForm):
     class Meta:
         model = Candidate
-        fields = ('end', 'user', 'salario', 'experiencia', 'scolariodade')
+        fields = ('user', 'salario', 'experiencia', 'escolaridade')
